ec9e1eb3f882398581e0e6b7f4c2e5e2366f3351
juvina <hello@jenniferuvina.com>
2024-08-27 20:54:47 -0600

    Add message history to model responses

--- app.py
+++ app.py
@@ -16,12 +16,17 @@ model_kwargs = {
 
 @cl.on_message
 async def on_message(message: cl.Message):
+    # Maintain an array of messages in the user session
+    message_history = cl.user_session.get("message_history", [])
+    # add in user information and current message for ease of use in API call
+    message_history.append({"role": "user", "content": message.content})
+
     # send initial response as an empty string
     response_message = cl.Message(content="")
     await response_message.send()
 
-    # set up model to stream response based on message
-    stream = await client.chat.completions.create(messages=[{"role": "user", "content": message.content}],
+    # send message history along to model and stream response
+    stream = await client.chat.completions.create(messages=message_history,
                                                   stream=True, **model_kwargs)
     # send over parts of response as stream as they're ready
     async for part in stream:
@@ -29,5 +34,9 @@ async def on_message(message: cl.Message):
             await response_message.stream_token(token)
     # finish the response message; no more updates
     await response_message.update()
+
+    # Record the AI's response in the history
+    message_history.append({"role": "assistant", "content": response_message.content})
+    cl.user_session.set("message_history", message_history)
 
 
